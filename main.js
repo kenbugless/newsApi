b7161e21147667e4aff16a33305d958ec45e5a72
kenbugless <kenbugless@gmail.com>
2024-02-21 22:49:15 +0900

    [update]:change new api from codingnoona

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-const API_KEY = `9c085f3a3ead4e4fad59ed05894efdaa`
-let news =[];
+// const API_KEY = `9c085f3a3ead4e4fad59ed05894efdaa`;
+let news = [];
 
 const getLatestNews = async () => {
-    let url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
-    const response = await fetch(url); 
-    console.log('response',response)
-    const data = await response.json();
-    let news = data.articles;
-    console.log('news는',news)
-  
-}
+	let url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
+	const response = await fetch(url);
+	console.log('response', response);
+	const data = await response.json();
+	let news = data.articles;
+	console.log('news는', news);
+};
 
-
-getLatestNews()
+getLatestNews();
