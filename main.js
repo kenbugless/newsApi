c0009ce5557980b7347a8ed976990b01f3914f23
kenbugless <kenbugless@gmail.com>
2024-02-21 14:50:31 +0900

    url 변경

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY = `9c085f3a3ead4e4fad59ed05894efdaa`
 let news =[];
 
 const getLatestNews = async () => {
-    let url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    let url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
     const response = await fetch(url); 
     console.log('response',response)
     const data = await response.json();
